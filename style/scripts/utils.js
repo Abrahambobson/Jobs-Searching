50e59e806d9760df55cc8bbf37befdd5ac12c0fc
Abrahambobson <abrahambturay@gmail.com>
2025-03-25 17:40:18 -0700

    Initial commit

---
+++ style/scripts/utils.js
@@ -0,0 +1,18 @@
+const loader = document.getElementById('loader');
+
+export async function getAllRemoteJobs(search_term = null) {
+    let jobs = fetch(`https://jobicy.com/api/v2/remote-jobs${ search_term ? '?tag=' + search_term.toString().toLowerCase() : '/'}`)
+    .then(res => res.json())
+    .then(data => {
+        let jobs = data.jobs
+        loader.classList.add('hidden');
+        return jobs;
+    })
+    .catch(error => {
+        console.log(error);
+        return 0;
+    })
+
+    return jobs;
+}
+
